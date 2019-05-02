98ac180a7ad2acadaca35e2a0cb6ce8ff717dbcb
Peng Wang <penwang@nvidia.com>
2019-05-02 16:26:13 -0700

    add 2-5 orders 3D cases

--- cuda/cuda/kernels/mass/multAdd.cpp
+++ cuda/cuda/kernels/mass/multAdd.cpp
@@ -28,8 +28,32 @@ void rMassMultAdd2D_v2(const int numElements,
                     double* restrict solOut)
 {
   const int e = blockIdx.x;
-  __shared__ double sol_xy[NUM_QUAD_1D][NUM_QUAD_1D];
-  __shared__ double temp[NUM_QUAD_1D][NUM_QUAD_1D];
+  __shared__ double buf1[NUM_QUAD_1D][NUM_QUAD_1D];
+  __shared__ double buf2[NUM_QUAD_1D][NUM_QUAD_1D];
+  __shared__ double matrix[NUM_QUAD_1D][NUM_QUAD_1D];
+  double (*sol_x)[NUM_QUAD_1D];
+  double (*sol_xy)[NUM_QUAD_1D];
+  double (*input)[NUM_QUAD_1D];
+
+  input = buf1;
+  sol_x = buf2;
+  sol_xy = buf1;
+
+  for (int dy = threadIdx.y; dy < NUM_DOFS_1D; dy += blockDim.y)
+  {
+    for (int dx = threadIdx.x; dx < NUM_DOFS_1D; dx += blockDim.x)
+    {
+      input[dy][dx] = solIn[ijkN(dx,dy,e,NUM_DOFS_1D)];
+    }
+  }
+  for (int dx = threadIdx.y; dx < NUM_DOFS_1D; dx += blockDim.y)
+  {
+    for (int qx = threadIdx.x; qx < NUM_QUAD_1D; qx += blockDim.x)
+    {
+      matrix[dx][qx] = dofToQuad[ijN(qx,dx,NUM_QUAD_1D)];
+    }
+  }  
+  __syncthreads();
   
   for (int dy = threadIdx.y; dy < NUM_DOFS_1D; dy += blockDim.y)
   {
@@ -38,9 +62,9 @@ void rMassMultAdd2D_v2(const int numElements,
       double t = 0;
       for (int dx = 0; dx < NUM_DOFS_1D; ++dx)
       {
-        t += dofToQuad[ijN(qx,dx,NUM_QUAD_1D)]*solIn[ijkN(dx,dy,e,NUM_DOFS_1D)];
+        t += matrix[dx][qx]*input[dy][dx];
       }
-      temp[dy][qx] = t;
+      sol_x[dy][qx] = t;
     }
   }
   __syncthreads();
@@ -50,7 +74,7 @@ void rMassMultAdd2D_v2(const int numElements,
     {
       double t = 0;
       for (int dy = 0; dy < NUM_DOFS_1D; ++dy) {
-        t += dofToQuad[ijN(qy,dy,NUM_QUAD_1D)]*temp[dy][qx];
+        t += dofToQuad[ijN(qy,dy,NUM_QUAD_1D)]*sol_x[dy][qx];
       }
       sol_xy[qy][qx] = t;
     }
@@ -63,6 +87,13 @@ void rMassMultAdd2D_v2(const int numElements,
       sol_xy[qy][qx] *= oper[ijkN(qx,qy,e,NUM_QUAD_1D)];
     }
   }
+  for (int qx = threadIdx.y; qx < NUM_QUAD_1D; qx += blockDim.y)
+  {
+    for (int dx = threadIdx.x; dx < NUM_DOFS_1D; dx += blockDim.x)
+    {
+      matrix[qx][dx] = quadToDof[ijN(dx,qx,NUM_DOFS_1D)];
+    }
+  }  
   __syncthreads();
 
   for (int qy = threadIdx.y; qy < NUM_QUAD_1D; qy += blockDim.y)  
@@ -72,9 +103,9 @@ void rMassMultAdd2D_v2(const int numElements,
       double t = 0;
       for (int qx = 0; qx < NUM_QUAD_1D; ++qx)
       {
-        t += quadToDof[ijN(dx,qx,NUM_DOFS_1D)] * sol_xy[qy][qx];
+        t += matrix[qx][dx] * sol_xy[qy][qx];
       }
-      temp[qy][dx] = t;
+      sol_x[qy][dx] = t;
     }
   }
   __syncthreads();
@@ -86,7 +117,7 @@ void rMassMultAdd2D_v2(const int numElements,
       double t = 0;
       for (int qy = 0; qy < NUM_QUAD_1D; ++qy)
       {    
-        t += quadToDof[ijN(dy,qy,NUM_DOFS_1D)] * temp[qy][dx];        
+        t += quadToDof[ijN(dy,qy,NUM_DOFS_1D)] * sol_x[qy][dx];        
       }
       solOut[ijkN(dx,dy,e,NUM_DOFS_1D)] = t;
     }
@@ -518,19 +549,48 @@ void rMassMultAdd3D_v2(const int numElements,
                        double* __restrict solOut)
 {
   const int e = blockIdx.x;
-  __shared__ double sol_xyz[NUM_QUAD_1D][NUM_QUAD_1D][NUM_QUAD_1D];
-  __shared__ double sol_xy[NUM_QUAD_1D][NUM_QUAD_1D][NUM_QUAD_1D];
-  __shared__ double sol_x[NUM_QUAD_1D][NUM_QUAD_1D][NUM_QUAD_1D];   
+  __shared__ double buf1[NUM_QUAD_1D][NUM_QUAD_1D][NUM_QUAD_1D];
+  __shared__ double buf2[NUM_QUAD_1D][NUM_QUAD_1D][NUM_QUAD_1D];
+  __shared__ double matrix[NUM_QUAD_1D][NUM_QUAD_1D];
+  double (*sol_xyz)[NUM_QUAD_1D][NUM_QUAD_1D];
+  double (*sol_xy)[NUM_QUAD_1D][NUM_QUAD_1D];
+  double (*sol_x)[NUM_QUAD_1D][NUM_QUAD_1D];
+  double (*input)[NUM_QUAD_1D][NUM_QUAD_1D];
+  input = buf2;
+  sol_x = buf1;
+  sol_xy = buf2;
+  sol_xyz = buf1;
   for (int dz = threadIdx.z; dz < NUM_DOFS_1D; dz += blockDim.z)
   {
     for (int dy = threadIdx.y; dy < NUM_DOFS_1D; dy += blockDim.y)
     {
-      for (int qx = threadIdx.x; qx < NUM_QUAD_1D; qx += blockDim.z)
+      for (int dx = threadIdx.x; dx < NUM_DOFS_1D; dx += blockDim.x)
+      {
+        input[dz][dy][dx] = solIn[ijklN(dx,dy,dz,e,NUM_DOFS_1D)];
+      }
+    }
+  }
+  if (threadIdx.z == 0)
+  {
+    for (int dx = threadIdx.y; dx < NUM_DOFS_1D; dx += blockDim.y)
+    {
+      for (int qx = threadIdx.x; qx < NUM_QUAD_1D; qx += blockDim.x)
+      {
+        matrix[dx][qx] = dofToQuad[ijN(qx,dx,NUM_QUAD_1D)];
+      }
+    }
+  }
+  __syncthreads();
+  for (int dz = threadIdx.z; dz < NUM_DOFS_1D; dz += blockDim.z)
+  {
+    for (int dy = threadIdx.y; dy < NUM_DOFS_1D; dy += blockDim.y)
+    {
+      for (int qx = threadIdx.x; qx < NUM_QUAD_1D; qx += blockDim.x)
       {
         double t = 0;
         for (int dx = 0; dx < NUM_DOFS_1D; ++dx)
         {
-          t += dofToQuad[ijN(qx,dx,NUM_QUAD_1D)] * solIn[ijklN(dx,dy,dz,e,NUM_DOFS_1D)];
+          t += matrix[dx][qx] * input[dz][dy][dx];          
         }
         sol_x[dz][dy][qx] = t;
       }
@@ -546,7 +606,7 @@ void rMassMultAdd3D_v2(const int numElements,
         double t = 0;
         for (int dy = 0; dy < NUM_DOFS_1D; ++dy)
         {
-          t += dofToQuad[ijN(qy,dy,NUM_QUAD_1D)] * sol_x[dz][dy][qx];
+          t += matrix[dy][qy] * sol_x[dz][dy][qx];
         }
         sol_xy[dz][qy][qx] = t;
       }
@@ -562,7 +622,7 @@ void rMassMultAdd3D_v2(const int numElements,
         double t = 0;
         for (int dz = 0; dz < NUM_DOFS_1D; ++dz)
         {
-          t += dofToQuad[ijN(qz,dz,NUM_QUAD_1D)] * sol_xy[dz][qy][qx];
+          t += matrix[dz][qz] * sol_xy[dz][qy][qx];
         }
         sol_xyz[qz][qy][qx] = t;
       }
@@ -580,8 +640,20 @@ void rMassMultAdd3D_v2(const int numElements,
       }
     }
   }
+  if (threadIdx.z == 0)
+  {
+    for (int qx = threadIdx.y; qx < NUM_QUAD_1D; qx += blockDim.y)
+    {
+      for (int dx = threadIdx.x; dx < NUM_DOFS_1D; dx += blockDim.x)
+      {
+        matrix[qx][dx] = quadToDof[ijN(dx,qx,NUM_DOFS_1D)];
+      }
+    }
+  }
   __syncthreads();
 
+  sol_x = buf2;
+  sol_xy = buf1;
   for (int qz = threadIdx.z; qz < NUM_QUAD_1D; qz += blockDim.z)
   {
     for (int qy = threadIdx.y; qy < NUM_QUAD_1D; qy += blockDim.y)
@@ -591,7 +663,7 @@ void rMassMultAdd3D_v2(const int numElements,
         double t = 0;
         for (int qx = 0; qx < NUM_QUAD_1D; ++qx)
         {
-          t += quadToDof[ijN(dx,qx,NUM_DOFS_1D)] * sol_xyz[qz][qy][qx];
+          t += matrix[qx][dx] * sol_xyz[qz][qy][qx];          
         }
         sol_x[qz][qy][dx] = t;
       }
@@ -607,7 +679,7 @@ void rMassMultAdd3D_v2(const int numElements,
         double t = 0;
         for (int qy = 0; qy < NUM_QUAD_1D; ++qy)
         {
-          t += quadToDof[ijN(dy,qy,NUM_DOFS_1D)] * sol_x[qz][qy][dx];
+          t += matrix[qy][dy] * sol_x[qz][qy][dx];          
         }
         sol_xy[qz][dy][dx] = t;
       }
@@ -620,10 +692,12 @@ void rMassMultAdd3D_v2(const int numElements,
     {
       for (int dx = threadIdx.x; dx < NUM_DOFS_1D; dx += blockDim.x)
       {
+        double t = solOut[ijklN(dx,dy,dz,e,NUM_DOFS_1D)];
         for (int qz = 0; qz < NUM_QUAD_1D; ++qz)
         {
-          solOut[ijklN(dx,dy,dz,e,NUM_DOFS_1D)] += quadToDof[ijN(dz,qz,NUM_DOFS_1D)] * sol_xy[qz][dy][dx];
+          t += matrix[qz][dz] * sol_xy[qz][dy][dx];          
         }
+        solOut[ijklN(dx,dy,dz,e,NUM_DOFS_1D)] = t;
       }
     }
   }
@@ -653,15 +727,13 @@ void rMassMultAdd(const int DIM,
                   const double* x,
                   double* __restrict y)
 {
-   const int blck = 256;
-   const int grid = (numElements+blck-1)/blck;
    assert(LOG2(DIM)<=4);
    assert((NUM_QUAD_1D&1)==0);
    assert(LOG2(NUM_DOFS_1D-1)<=8);
    assert(LOG2(NUM_QUAD_1D>>1)<=8);
    const unsigned int id = (DIM<<16)|((NUM_DOFS_1D-1)<<8)|(NUM_QUAD_1D>>1);
-   // static std::unordered_map<unsigned int, fMassMultAdd> call =
-   // {
+   static std::unordered_map<unsigned int, fMassMultAdd> call =
+   {
    //    // 2D
    //    {0x20001,&rMassMultAdd2D<1,2>},    {0x20101,&rMassMultAdd2D<2,2>},
    //    {0x20102,&rMassMultAdd2D<2,4>},    {0x20202,&rMassMultAdd2D<3,4>},
@@ -681,10 +753,10 @@ void rMassMultAdd(const int DIM,
    //    {0x20F10,&rMassMultAdd2D<16,32>},  {0x21010,&rMassMultAdd2D<17,32>},
    //    // 3D
    //    {0x30001,&rMassMultAdd3D<1,2>},    {0x30101,&rMassMultAdd3D<2,2>},
-   //    {0x30102,&rMassMultAdd3D<2,4>},    {0x30202,&rMassMultAdd3D<3,4>},
+      // {0x30102,&rMassMultAdd3D<2,4>},    {0x30202,&rMassMultAdd3D<3,4>},
    //    {0x30203,&rMassMultAdd3D<3,6>},    {0x30303,&rMassMultAdd3D<4,6>},
    //    {0x30304,&rMassMultAdd3D<4,8>},    {0x30404,&rMassMultAdd3D<5,8>},
-   //    {0x30405,&rMassMultAdd3D<5,10>},   {0x30505,&rMassMultAdd3D<6,10>},
+      {0x30405,&rMassMultAdd3D<5,10>},   {0x30505,&rMassMultAdd3D<6,10>},
    //    {0x30506,&rMassMultAdd3D<6,12>},   {0x30606,&rMassMultAdd3D<7,12>},
    //    {0x30607,&rMassMultAdd3D<7,14>},   {0x30707,&rMassMultAdd3D<8,14>},
    //    {0x30708,&rMassMultAdd3D<8,16>},   {0x30808,&rMassMultAdd3D<9,16>},
@@ -696,33 +768,74 @@ void rMassMultAdd(const int DIM,
    //    {0x30D0E,&rMassMultAdd3D<14,28>},  {0x30E0E,&rMassMultAdd3D<15,28>},
    //    {0x30E0F,&rMassMultAdd3D<15,30>},  {0x30F0F,&rMassMultAdd3D<16,30>},
    //    {0x30F10,&rMassMultAdd3D<16,32>},  {0x31010,&rMassMultAdd3D<17,32>},
-   // };
-   static std::unordered_map<unsigned int, fMassMultAdd> call =
-   {
-     {0x20304,&rMassMultAdd2D<4,8>},    {0x20404,&rMassMultAdd2D<5,8>},
-     {0x30304,&rMassMultAdd3D<4,8>},    {0x30404,&rMassMultAdd3D<5,8>},      
-   };   
+   };
+
+#define call_3d(d,q) rMassMultAdd3D_v2<d,q><<<grid,blck>>>( \
+     numElements,dofToQuad,dofToQuadD,quadToDof,quadToDofD,op,x,y)
    
-   if (!call[id])
-   {
-      printf("\n[rMassMultAdd] id \033[33m0x%X\033[m ",id);
-      fflush(stdout);
-   }
-   assert(call[id]);
    static int call_no = 0;
-   if (id == 0x20404)
+   int grid = numElements;
+   if (DIM == 2 && NUM_DOFS_1D == 5 && NUM_QUAD_1D == 8)
    {
-     int grid = numElements;
      dim3 block(8,8,1);
      rMassMultAdd2D_v2<5,8><<<grid,block>>>(numElements,dofToQuad,dofToQuadD,quadToDof,quadToDofD,op,x,y);
-   } else if (id == 0x30404) {
-     int grid = numElements;
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 2 && NUM_QUAD_1D == 4)
+   {
+     dim3 blck(4,4,2);
+     call_3d(2,4);
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 3 && NUM_QUAD_1D == 4)
+   {
+     dim3 blck(4,4,4);
+     call_3d(3,4);
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 3 && NUM_QUAD_1D == 6)
+   {
+     dim3 blck(6,6,3);
+     call_3d(3,6);
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 4 && NUM_QUAD_1D == 6)
+   {
+     dim3 blck(6,6,4);
+     call_3d(4,6);
+   }   
+   else if (DIM == 3 && NUM_DOFS_1D == 4 && NUM_QUAD_1D == 8)
+   {
      dim3 blck(8,8,2);
-     rMassMultAdd3D_v2<5,8><<<grid,blck>>>(numElements,dofToQuad,dofToQuadD,quadToDof,quadToDofD,op,x,y);
-   } else {
-   call0(id,grid,blck,
-         numElements,dofToQuad,dofToQuadD,quadToDof,quadToDofD,op,x,y);
+     call_3d(4,8);
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 5 && NUM_QUAD_1D == 8)
+   {
+     dim3 blck(8,8,2);
+     call_3d(5,8);
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 5 && NUM_QUAD_1D == 10)
+   {
+     dim3 blck(10,10,2);
+     call_3d(5,10);
+   }
+   else if (DIM == 3 && NUM_DOFS_1D == 6 && NUM_QUAD_1D == 10)
+   {
+     dim3 blck(10,10,2);
+     call_3d(6,10);
+   }   
+   else
+   {
+     if (!call[id])
+     {
+       printf("\n[rMassMultAdd] id \033[33m0x%X\033[m ",id);
+       fflush(stdout);
+     }
+     assert(call[id]);     
+     const int blck = 256;
+     const int grid = (numElements+blck-1)/blck;     
+     call0(id,grid,blck,
+           numElements,dofToQuad,dofToQuadD,quadToDof,quadToDofD,op,x,y);
    }
    CUCHK(cudaGetLastError());
+   // cudaDeviceSynchronize();
+   // cudaDeviceReset();
+   // exit(0);
 }
  
